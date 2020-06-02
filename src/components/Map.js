7b4de1444f5262450f7e2e2a5e870fac0a01c203
Sisir K Adhikari <sisir@tutremix.com>
2020-06-02 15:04:04 +0600

    Use react hooks

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, InfoBox } from '@react-google-maps/api'
 
 import axios from 'axios';
 
-import { ultraLight, shadesOfGrey } from '../utils/mapStyles';
+import { shadesOfGrey } from '../utils/mapStyles';
 import Marker from './CircelMarker';
 import { CircularProgress } from '@material-ui/core';
 
@@ -13,66 +13,57 @@ const key = process.env.REACT_APP_GOOGLE_API_KEY;
 // const defaultCenter = { lat: 23.797911, lng: 90.414391 }; // Dhaka/Bangladesh
 const defaultCenter = {lat: -33.865427, lng: 151.196123};
 
-class Map extends React.Component {
+const Map = () => {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            data: [],
-        }
-    }
-
-    componentDidMount(){
-        this.updateData();
+    const [ data, setData ] = useState([]);
+    const [ hoveredItem, setHoveredItem ] = useState(null);
+    
+    useEffect(() => {
+        updateData();
         setInterval(this.updateData, 60 * 1000);
-    }
+    }, []);
 
-    updateData = async () => {
+    const updateData = async () => {
         const res = await axios.get(url);
         const data = res.data.features.filter(v => v.properties.mag >= 2);
-
-        this.setState({data: data});
+        setData(data);
     }
 
-    onHoverItem = item => {
-        this.setState({hoveredItem: item});
+    const onHoverItem = item => {
+        setHoveredItem(item);
     }
 
-    onBlurItem = () => {
-        this.setState({hoveredItem: null});
+    const onBlurItem = () => {
+        setHoveredItem(null);
     }
-
-    render(){
-        return (
-            <LoadScript
-                id="script-loader"
-                googleMapsApiKey={key}
-                loadingElement={<CircularProgress />}
-                
-            >
-                <GoogleMap
-                    id="eq-map"
-                    mapContainerStyle={{height: '100vh'}}
-                    zoom={4}
-                    center={defaultCenter}
-                    options={{
-                        styles: shadesOfGrey,
-                        streetViewControl: false,
-                        fullscreenControl: false,
-                        mapTypeControl: false,
-                    }}
-                >  
-                    <React.Fragment>
-                        
-                    
-                        {this.state.data.length && this.state.data.map(d => {
-                            return <Marker key={d.id} onBlur={this.onBlurItem} onHover={e => this.onHoverItem(d, e)} magnitude={d.properties.mag} position={{ lat: d.geometry.coordinates[1], lng: d.geometry.coordinates[0] }} />
-                        })}
-                    </React.Fragment> 
-                </GoogleMap>
-            </LoadScript>
-        );
-    }
+    
+    return (
+        <LoadScript
+            id="script-loader"
+            googleMapsApiKey={key}
+            loadingElement={<CircularProgress />}
+            
+        >
+            <GoogleMap
+                id="eq-map"
+                mapContainerStyle={{height: '100vh'}}
+                zoom={4}
+                center={defaultCenter}
+                options={{
+                    styles: shadesOfGrey,
+                    streetViewControl: false,
+                    fullscreenControl: false,
+                    mapTypeControl: false,
+                }}
+            >  
+                <>
+                    {data.length && data.map(d => {
+                        return <Marker key={d.id} onBlur={onBlurItem} onHover={e => onHoverItem(d, e)} magnitude={d.properties.mag} position={{ lat: d.geometry.coordinates[1], lng: d.geometry.coordinates[0] }} />
+                    })}
+                </> 
+            </GoogleMap>
+        </LoadScript>
+    );
 }
 
 export default Map;
--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Parser from 'rss-parser';
 import { Drawer, Divider, IconButton } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,44 +9,39 @@ const parser = new Parser(
         headers: {'origin': 'EQ Monitor'}
     }
 );
-const feedUrl = 'http://www.earthquakenewstoday.com/feed';
+const feedUrl = process.env.REACT_APP_NEWS_FEED_URL;
 
-class News extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            news: [],
-        }
-    }
-    componentWillMount(){
-        this.parseFeed();
-    }
+const News = () => {
 
-    parseFeed = async () => {
+    const [ news, setNews ] = useState([]);
+
+    useEffect(() => {
+        parseFeed();
+    }, []);
+
+    const parseFeed = async () => {
         const feeds = await parser.parseURL(feedUrl);
         console.log(feeds);
     }
 
-    render(){
-    
-        return (
-            <Drawer
-                open={this.props.open}
-                anchor="right"
-            >
-                <div>
-                    <IconButton onClick={this.props.onCloseNews}>
-                        {this.props.open ? <FontAwesomeIcon icon={faChevronRight} /> : <FontAwesomeIcon icon={faChevronLeft} />}
-                    </IconButton>
-                </div>
-                
-                <Divider />
 
-                <h1>News</h1>
-                
-            </Drawer>
-      );
-    }
+    return (
+        <Drawer
+            open={this.props.open}
+            anchor="right"
+        >
+            <div>
+                <IconButton onClick={this.props.onCloseNews}>
+                    {this.props.open ? <FontAwesomeIcon icon={faChevronRight} /> : <FontAwesomeIcon icon={faChevronLeft} />}
+                </IconButton>
+            </div>
+            
+            <Divider />
+
+            <h1>News</h1>
+            
+        </Drawer>
+    );
 }
 
 export default News;
